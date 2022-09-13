fc8bfe09b2f84dd03d33ffa787252762aa57fca3
yushulx <lingxiao1002@gmail.com>
2022-09-13 14:55:53 +0800

    Add camera capture

--- projects/ngx-web-document-scanner/src/lib/ngx-camera-capture/ngx-camera-capture.component.ts
+++ projects/ngx-web-document-scanner/src/lib/ngx-camera-capture/ngx-camera-capture.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { WebTwain } from 'dwt/dist/types/WebTwain'; 
+import Dynamsoft from 'dwt';
 
 @Component({
   selector: 'ngx-camera-capture',
@@ -7,20 +8,84 @@ import { WebTwain } from 'dwt/dist/types/WebTwain';
   styleUrls: ['./ngx-camera-capture.component.css']
 })
 export class NgxCameraCaptureComponent implements OnInit {
-  @Input() showOverlay: boolean;
-  isLoaded = false;
-  overlay: HTMLCanvasElement | undefined;
-  context: CanvasRenderingContext2D | undefined;
-  cameraInfo: any = {};
+  dwtObject: WebTwain | undefined;
   videoSelect: HTMLSelectElement | undefined;
+  sourceDict: any = {};
 
-  @Output() result = new EventEmitter<any>();
-
-  constructor() {
-    this.showOverlay = true;
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    
+    this.videoSelect = document.querySelector('select#videoSource') as HTMLSelectElement;
+    Dynamsoft.DWT.Containers = [{ ContainerId: 'dwtcontrolContainer' }];
+    Dynamsoft.DWT.UseLocalService = false;
+    Dynamsoft.DWT.Load();
+    Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', () => { this.onReady(); });
+  }
+
+  scanDocument() {
+    if (this.videoSelect) {
+      let index = this.videoSelect.selectedIndex;
+      if (index < 0) return;
+
+      var option = this.videoSelect.options[index];
+      if (this.dwtObject) {
+        this.dwtObject.Addon.Camera.selectSource(this.sourceDict[option.text]).then(camera => {
+          if (this.videoSelect) this.createCameraScanner(this.sourceDict[option.text]);
+        });
+      }
+
+    }
+
+  }
+
+  downloadDocument() {
+    if (this.dwtObject) {
+      this.dwtObject.SaveAsJPEG("document.jpg", this.dwtObject.CurrentImageIndexInBuffer);
+    }
+  }
+
+  onReady() {
+    this.dwtObject = Dynamsoft.DWT.GetWebTwain('dwtcontrolContainer');
+    this.updateCameraList();
+  }
+
+  async createCameraScanner(deviceId: string): Promise<void> {
+    if (this.dwtObject) {
+      await this.dwtObject.Addon.Camera.closeVideo();
+      this.dwtObject.Addon.Camera.scanDocument({
+        scannerViewer: {
+          deviceId: deviceId,
+          fullScreen: true,
+          autoDetect: {
+            enableAutoDetect: true
+          },
+          continuousScan: true
+        }
+
+      }).then(
+        function () { console.log("OK"); },
+        function (error: any) { console.log(error.message); });
+    }
+
+  }
+
+  updateCameraList() {
+    if (this.videoSelect && this.dwtObject) {
+      this.videoSelect.options.length = 0;
+      this.dwtObject.Addon.Camera.getSourceList().then((list) => {
+        for (var i = 0; i < list.length; i++) {
+          var option = document.createElement('option');
+          option.text = list[i].label || list[i].deviceId;
+          if (list[i].label) {
+            this.sourceDict[list[i].label] = list[i].deviceId;
+          }
+          else {
+            this.sourceDict[list[i].deviceId] = list[i].deviceId;
+          }
+          if (this.videoSelect) this.videoSelect.options.add(option);
+        }
+
+      });
+    }
   }
 }
